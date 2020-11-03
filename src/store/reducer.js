b7d824675218ec3f287cec02756593f4c7202ba0
sne-syn <snezhana.syngur@gmail.com>
2020-11-03 19:34:53 +0100

    fix: Typo

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,11 +4,11 @@ import {ActionType} from "./action";
 import {generateOffers} from '../mocks/offers.js';
 import {filterData, filterFavorites} from './../core';
 const offers = generateOffers(20);
-const DEFUALT_CITY = CITIES[0];
+const DEFAULT_CITY = CITIES[0];
 const initialState = {
   offers,
-  activeElement: DEFUALT_CITY,
-  filteredOffers: filterData(offers, DEFUALT_CITY),
+  activeElement: DEFAULT_CITY,
+  filteredOffers: filterData(offers, DEFAULT_CITY),
 };
 
 const reducer = (state = initialState, action) => {
