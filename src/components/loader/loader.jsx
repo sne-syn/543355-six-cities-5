d90f6f25fbd0a3480c6ceab6e3c3222e6181ccf8
sne-syn <snezhana.syngur@gmail.com>
2020-11-17 23:51:11 +0100

    new: Update loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -3,7 +3,11 @@ import './loader.css';
 
 const Loader = () => {
   return (
-    <div className="loader" id="loader"></div>
+    <div className="loader" id="loader">
+    <div class="loadingio-spinner-eclipse-tdjcbvvh5xi"><div class="ldio-hnl6tyhogo8">
+<div></div>
+</div></div>
+    </div>
   );
 };
 
