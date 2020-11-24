c23b779a78bb36abc085a50697acda0f94aecf27
sne-syn <snezhana.syngur@gmail.com>
2020-11-24 20:48:04 +0100

    new: Small changes

--- src/components/host/host.jsx
+++ src/components/host/host.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 
 const Host = ({description, host}) => {
   const {isPro, avatar, name} = host;
+  const isProMark = isPro ? `property__avatar-wrapper--pro` : ``;
   return (
     <div className="property__host">
       <h2 className="property__host-title">Meet the host</h2>
       <div className="property__host-user user">
-        <div className={`property__avatar-wrapper ${isPro && (`property__avatar-wrapper--pro`)} user__avatar-wrapper`}>
+        <div className={`property__avatar-wrapper ${isProMark}`}>
           <img className="property__avatar user__avatar" src={`/${avatar}`} width="74" height="74" alt="Host avatar" />
         </div>
         <span className="property__user-name">
