93ed573a02ac561797066f2f0ec5061fd157bb77
sne-syn <36764312+sne-syn@users.noreply.github.com>
2020-11-26 15:39:04 +0100

    Update app.test.js

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from './app';
+import {App} from './app';
 
 jest.mock(`react-router-dom`, () => ({Link: `Link`}));
 
