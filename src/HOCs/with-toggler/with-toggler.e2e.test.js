050723276fe95d99ec1a3e4ffc53bdbac75fec56
sne-syn <snezhana.syngur@gmail.com>
2020-11-29 21:04:03 +0100

    new: Update wothInputs hoc test

--- src/HOCs/with-toggler/with-toggler.e2e.test.js
+++ src/HOCs/with-toggler/with-toggler.e2e.test.js
@@ -4,16 +4,24 @@ import Adapter from 'enzyme-adapter-react-16';
 import withToggler from './with-toggler';
 
 configure({adapter: new Adapter()});
-
+const noop = () => {};
 const MockComponent = () => <div />;
 const MockComponentWrapped = withToggler(MockComponent);
 const onDefaultState = false;
 const onToggleComponent = jest.fn();
+const component = <MockComponentWrapped toggleComponent={noop}/>
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(component);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+});
 
 describe(`WithToggler checks`, () => {
   it(`Should on defaultState eq 'false'`, () => {
-    const wrapper = shallow(<MockComponentWrapped toggleComponent={() => {}}/>);
-
     expect(wrapper.state().on).toEqual(onDefaultState);
   });
   it(`Should recieve state from Component and be eq 'true`, () => {
@@ -23,6 +31,6 @@ describe(`WithToggler checks`, () => {
           toggleComponent={onToggleComponent}
         />
     );
-    expect(wrapper.props().on).toEqual(true);
+    expect(wrapper.state().on).toEqual(true);
   });
 });
